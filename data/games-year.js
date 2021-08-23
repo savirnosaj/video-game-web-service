bf07623190581cbf5f5d3f24b7e02de195ba9d92
savir_nosaj <jason.rivas@bellevuecollege.edu>
2021-08-22 21:31:35 -0700

    update

--- data/games-year.js
+++ data/games-year.js
@@ -1,45 +1,138 @@
 {
-	"title": "Video Games by Year",
-	"films":[
-                {
-                    "Title":"Sonic The Hedgehog",
-                    "Genre":"Platformer",
-                    "Company":"Sega",
-                    "Year":1991,
-                    "Rating":"E",
-                    "Image":"sonic-the-hedgehog.jpg"
-                },
-                {
-                    "Title":"Super Mario Kart",
-                    "Genre":"Racing",
-                    "Company":"Nintendo",
-                    "Year":1992,
-                    "Rating":"E",
-                    "Image":"super-mario-kart.jpg"
-                },
-                {
-                    "Title":"Portal",
-                    "Genre":"Puzzle",
-                    "Company":"Valve",
-                    "Year":2007,
-                    "Rating":"T",
-                    "Image":"portal.jpg"
-                },
-                {
-                    "Title":"Minecraft",
-                    "Genre":"Sandbox",
-                    "Company":"Mojang",
-                    "Year":2011,
-                    "Rating":"E(+10)",
-                    "Image":"minecraft.jpg"
-                },
-                {
-                    "Title":"Counter Strike | Global Offensive",
-                    "Genre":"Tactical Shooter",
-                    "Company":"Valve",
-                    "Year":2012,
-                    "Rating":"M",
-                    "Image":"csgo.jpg"
-                },
+	"title": "Harry Potter Films by Box Office Totals",
+	"films":
+	[
+		{
+			"Film":1,
+			"Title":"Harry Potter and the Philosopher's Stone",
+			"Year":2001,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter1.jpg"
+		},
+		{
+			"Film":2,
+			"Title":"Harry Potter and the Chamber of Secrets",
+			"Year":2002,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter2.jpg"
+		},
+		{
+			"Film":3,
+			"Title":"Harry Potter and the Prisoner of Azkaban",
+			"Year":2004,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter3.jpg"
+		},
+		{
+			"Film":4,
+			"Title":"Harry Potter and the Goblet of Fire",
+			"Year":2005,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter4.jpg"
+		},
+		{
+			"Film":5,
+			"Title":"Harry Potter and the Order of the Phoenix",
+			"Year":2007,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter5.jpg"
+		},
+		{
+			"Film":6,
+			"Title":"Harry Potter and the Half-Blood Prince",
+			"Year":2009,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter6.jpg"
+		},
+		{
+			"Film":7.1,
+			"Title":"Harry Potter and the Deathly Hallows: Part 1",
+			"Year":2010,
+			"Director":"Chris Columbus",
+			"Producers":"Todd Arnow, Michael Barnathan, Chris Columbus",
+			"Writers":"J.K. Rowling (novel), Steve Kloves (screenplay)",
+			"Composer":"John Williams",
+			"Budget":"$125,000,000",
+			"BoxOffice":"$1,006,968,171",
+			"Image":"harry-potter7-1.jpg"
+		},
+		{
+			"Film":7.2,
+			"Title":"Harry Potter and the Deathly Hallows: Part 2",
+			"Year":2011,
+			"Director":"Lewis Gilbert",
+			"Producers":"Albert R. Broccoli",
+			"Writers":"Christopher Wood",
+			"Composer":"John Barry",
+			"Budget":"$180,000,000",
+			"BoxOffice":"$210,300,000.00",
+			"Image":"harry-potter7-2.jpg"
+		},
+		{
+			"Film":8,
+			"Title":"Fantastic Beasts and Where to Find Them",
+			"Year":2016,
+			"Director":"David Yates",
+			"Producers":"Albert R. Broccoli",
+			"Writers":"Christopher Wood",
+			"Composer":"John Barry",
+			"Budget":"$180,000,000",
+			"BoxOffice":"$814,044,001",
+			"Image":"fantastic-beasts1.jpg"
+		},
+		{
+			"Film":9,
+			"Title":"Fantastic Beasts and Where to Find Them 2",
+			"Year":2018,
+			"Director":"David Yates",
+			"Producers":"Albert R. Broccoli",
+			"Writers":"Christopher Wood",
+			"Composer":"John Barry",
+			"Budget":"$200,000,000",
+			"BoxOffice":"$654,855,901",
+			"Image":"fantastic-beasts2.jpg"
+		},
+		{
+			"Film":10,
+			"Title":"Fantastic Beasts and Where to Find Them 3",
+			"Year":2022,
+			"Director":"David Yates",
+			"Producers":"Albert R. Broccoli",
+			"Writers":"Christopher Wood",
+			"Composer":"John Barry",
+			"Budget":"$31,000,000.00",
+			"BoxOffice":"$210,300,000.00",
+			"Image":"fantastic-beasts3.jpg"
+		}
 	]
 }
--- index.php
+++ index.php
@@ -1,100 +1,107 @@
 <html>
-	<head>
-		<title>Video Game Web Service</title>
-		<style>
-			body {font-family:georgia;}
-			.film{
-				border:1px solid #E77DC2;
-				border-radius: 5px;
-				padding: 5px;
-				margin-bottom:5px;
-				position:relative;	
-			}
+<head>
+<title>Harry Potter Web Service Demo</title>
+<style>
+    body {font-family:georgia;}
+    .film {
+        border:1px solid #E77DC2;
+        border-radius: 5px;
+        padding: 5px;
+        margin-bottom:5px;
+        position:relative;
+    }
+    .pic {
+        position:absolute;
+        right:10px;
+        top:10px;
+    }
+</style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-			.pic{
-				position:absolute;
-				right:10px;
-				top:10px;
-			}
-
-		</style>
-		<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
-
-		<script type="text/javascript">
-			$(document).ready(function() {  
-
-				$('.category').click(function(e){
-					e.preventDefault(); //stop default action of the link
-					cat = $(this).attr("href");  //get category from URL
-					loadAJAX(cat);  //load AJAX and parse JSON file
-				});
-			});	
-
-
-			function loadAJAX(cat)
-			{
-				//AJAX connection will go here
-				//alert('cat is: ' + cat);
-				$.ajax({
-					type: "GET",
-					dataType: "json",
-					url: "api.php?cat=" + cat,
-					success: bondJSON
-				});
-			}
-				
-			function toConsole(data)
-			{//return data to console for JSON examination
-				console.log(data); //to view,use Chrome console, ctrl + shift + j
-			}
-
-			function bondJSON(data){
-			//JSON processing data goes here
-				console.log(data);
-
-				$('#filmTitle').html(data.title);
-
-				$('#films').html('');
-				
-				// $.each(data.games, function(i, item){
-				// 		let myGame = bondTemplate(item);
-
-				// 	$('<div></div>').html(myGame).appendTo('#films');
-				// });
-
-				//this loads the data on the page, but it's all bunched up
-				// $('#output').text(JSON.stringify(data));
-				
-				// this creates a map of the JSON on our page
-				let myData = JSON.stringify(data,null,4);
-				myData = "<pre>" + myData + "</pre>";
-				$("#output").html(myData);
-
-			}
-
-			function bondTemplate(games){
-				return `
-					<div class="film">
-						<b>Title: </b> ${games.Title} <br>
-						<b>Genre: </b> ${games.Genre} <br>
-						<b>Company: </b> ${games.Company} <br>
-						<b>Year: </b> ${games.Year} <br>
-						<b>Rating: </b> ${games.Rating} <br>
-						<div class="pic"> <img src="thumbnails/${games.Image}" alt=""></div>
-					</div>
-				`;
-			}
-
-		</script>
-	</head>
-
-	<body>
-		<h1>Video Game Web Service</h1>
-		<a href="year" class="category">Video Games By Year</a><br />
-		<a href="title" class="category">Video Games by Title</a>
-		<h3 id="filmtitle">Title Will Go Here</h3>
-		<div id="films"></div>
-		<div id="output">Results go here</div>
-	</body>
-
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.category').click(function(e){
+        e.preventDefault(); //stop default action of the link
+        cat = $(this).attr("href");  //get category from URL
+        loadAJAX(cat);  //load AJAX and parse JSON file
+    });
+});
+function loadAJAX(cat)
+{
+    //AJAX connection will go here
+    //alert('cat is: ' + cat);
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "api.php?cat=" + cat,
+        success: potterJSON
+    });
+}
+function toConsole(data)
+{//return data to console for JSON examination
+    console.log(data); //to view,use Chrome console, ctrl + shift + j
+}
+function potterJSON(data){
+// Here is how i see data returned via Console
+    console.log(data);
+    //indentifies the type of data returned
+    $('#filmtitle').html(data.title);
+    //clears other clicked films
+    $('#films').html("");
+    //loop through films and add template
+    $.each(data.films,function(i,item){
+        let myFilm = harryPotterTemplate(item);
+        $('<div></div>').html(myFilm).appendTo('#films');
+    });
+//This loads the data on the page, but it is all bunched
+//$("#output").text(JSON.stringify(data));
+//this creates a map of JSON on our page
+/*
+let myData = JSON.stringify(data,null,4);
+myData = "<pre>" + myData + "</pre>";
+$("#output").html(myData);
+*/
+}
+function harryPotterTemplate(film){
+    return `
+        <div class="film">
+            <b>Film:</b>${film.Film}<br />
+            <b>Title:</b>${film.Title}<br />
+            <b>Year:</b>${film.Year}<br />
+            <b>Director:</b>${film.Director}<br />
+            <b>Producers:</b>${film.Producers}<br />
+            <b>Writers:</b>${film.Writers}<br />
+            <b>Composer:</b>${film.Composer}<br />
+            <b>Budget:</b>${film.Budget}<br />
+            <b>Box Office:</b>${film.BoxOffice}<br />
+        <div class="pic">
+            <img width="100px" height="auto" src="thumbnails/${film.Image}" />
+        </div>
+    `;
+}
+</script>
+</head>
+    <body>
+    <h1>Harry Potter Web Service</h1>
+        <a href="year" class="category">Harry Potter Films By Year</a><br />
+        <a href="box" class="category">Harry Potter Films By International Box Office Totals</a>
+        <h3 id="filmtitle">Title Will Go Here</h3>
+        <div id="films">
+        <!--
+            <div class="film">
+                <b>Film:</b>1<br />
+                <b>Title:</b>Dr. No<br />
+                <b>Year:</b>1962<br />
+                <b>Director:</b>Terence Young<br />
+                <b>Producers:</b>Harry Saltzman and Albert R. Broccoli<br />
+                <b>Writers:</b>Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+                <b>Composer:</b>Monty Norman<br />
+                <b>Budget:</b>$1,000,000.00<br />
+                <b>Box Office:</b>$59,567,035.00<br />
+                <div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
+            </div>
+            -->
+        </div>
+        <div id="output">Results go here</div>
+    </body>
 </html>
